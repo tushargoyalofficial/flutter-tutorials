26feb09f1232964d5d158e0f64ab661c7218eaaf
Tushar Goyal <tushar462024@gmail.com>
2020-09-03 01:16:19 +0530

    Change title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Find Talent',
+      title: 'Flutter Tutorials',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
