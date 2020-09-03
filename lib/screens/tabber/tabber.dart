a6ce681704c7368e5a35fc1cfab0ad7dd1b9f081
Tushar Goyal <tushar.goyal@hiteshi.com>
2020-09-03 22:20:00 +0530

    Use StatefulWrapper inside Stateless widgets

--- lib/screens/tabber/tabber.dart
+++ lib/screens/tabber/tabber.dart
@@ -1,6 +1,7 @@
 import 'package:find_talent/screens/tabber/constants.dart';
 import 'package:find_talent/screens/tabber/tabber_body.dart';
 import 'package:find_talent/screens/tabber/tabber_provider.dart';
+import 'package:find_talent/widgets/stateful_wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -9,30 +10,34 @@ class TabberScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tabber'),
-        centerTitle: true,
-      ),
-      body: TabberBody(navigatorKey: _navigatorKey),
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(boxShadow: [
-          BoxShadow(
-              color: Colors.grey,
-              offset: Offset(0, 0),
-              blurRadius: 5.0,
-              spreadRadius: 1.0)
-        ]),
-        child: BottomNavigationBar(
-          currentIndex: context.watch<TabberProvider>().tabIndex,
-          items: allTabPages.map((TabData destination) {
-            return BottomNavigationBarItem(
-                icon: Icon(destination.icon),
-                backgroundColor: destination.color,
-                title: Text(destination.title, style: TextStyle(height: 1.4)));
-          }).toList(),
-          onTap: (int val) =>
-              context.read<TabberProvider>().onTabPress(val, _navigatorKey),
+    return StatefulWrapper(
+      onInit: () {},
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Tabber'),
+          centerTitle: true,
+        ),
+        body: TabberBody(navigatorKey: _navigatorKey),
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(boxShadow: [
+            BoxShadow(
+                color: Colors.grey,
+                offset: Offset(0, 0),
+                blurRadius: 5.0,
+                spreadRadius: 1.0)
+          ]),
+          child: BottomNavigationBar(
+            currentIndex: context.watch<TabberProvider>().tabIndex,
+            items: allTabPages.map((TabData destination) {
+              return BottomNavigationBarItem(
+                  icon: Icon(destination.icon),
+                  backgroundColor: destination.color,
+                  title:
+                      Text(destination.title, style: TextStyle(height: 1.4)));
+            }).toList(),
+            onTap: (int val) =>
+                context.read<TabberProvider>().onTabPress(val, _navigatorKey),
+          ),
         ),
       ),
     );
