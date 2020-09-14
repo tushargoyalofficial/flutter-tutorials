93f5bd7b3a061c108cc0eaa425109d3925e7712a
Tushar Goyal <tushar.goyal@hiteshi.com>
2020-09-14 22:34:24 +0530

    Create tab navigation multipage within bottom tabs

---
+++ lib/screens/faded_tabber/destination_view.dart
@@ -0,0 +1,37 @@
+import 'package:find_talent/screens/faded_tabber/tab_list.dart';
+import 'package:flutter/material.dart';
+import 'list_page.dart';
+import 'root_page.dart';
+import 'text_page.dart';
+
+class DestinationView extends StatefulWidget {
+  const DestinationView({Key key, this.destination}) : super(key: key);
+
+  final Destination destination;
+
+  @override
+  _DestinationViewState createState() => _DestinationViewState();
+}
+
+class _DestinationViewState extends State<DestinationView> {
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+      onGenerateRoute: (RouteSettings settings) {
+        return MaterialPageRoute(
+          settings: settings,
+          builder: (BuildContext context) {
+            switch (settings.name) {
+              case '/':
+                return RootPage(destination: widget.destination);
+              case '/list':
+                return ListPage(destination: widget.destination);
+              case '/text':
+                return TextPage(destination: widget.destination);
+            }
+          },
+        );
+      },
+    );
+  }
+}
---
+++ lib/screens/faded_tabber/faded_tabber.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'destination_view.dart';
+import 'tab_list.dart';
+
+class FadedTabber extends StatefulWidget {
+  @override
+  _FadedTabberState createState() => _FadedTabberState();
+}
+
+class _FadedTabberState extends State<FadedTabber>
+    with TickerProviderStateMixin<FadedTabber> {
+  List<Key> _destinationKeys;
+  List<AnimationController> _faders;
+  int _currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _faders =
+        allDestinations.map<AnimationController>((Destination destination) {
+      return AnimationController(
+          vsync: this, duration: Duration(milliseconds: 200));
+    }).toList();
+    _faders[_currentIndex].value = 1.0;
+    _destinationKeys =
+        List<Key>.generate(allDestinations.length, (int index) => GlobalKey())
+            .toList();
+  }
+
+  @override
+  void dispose() {
+    for (AnimationController controller in _faders) controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        top: false,
+        child: Stack(
+          fit: StackFit.expand,
+          children: allDestinations.map((Destination destination) {
+            final Widget view = FadeTransition(
+              opacity: _faders[destination.index]
+                  .drive(CurveTween(curve: Curves.fastOutSlowIn)),
+              child: KeyedSubtree(
+                key: _destinationKeys[destination.index],
+                child: DestinationView(
+                  destination: destination,
+                ),
+                // child: Center(
+                //     child: Container(
+                //   child: Text('center container'),
+                // )),
+              ),
+            );
+            if (destination.index == _currentIndex) {
+              _faders[destination.index].forward();
+              return view;
+            } else {
+              _faders[destination.index].reverse();
+              if (_faders[destination.index].isAnimating) {
+                return IgnorePointer(child: view);
+              }
+              return Offstage(child: view);
+            }
+          }).toList(),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: allDestinations.map((Destination destination) {
+          return BottomNavigationBarItem(
+              icon: Icon(destination.icon),
+              backgroundColor: destination.color,
+              title: Text(destination.title));
+        }).toList(),
+      ),
+    );
+  }
+}
